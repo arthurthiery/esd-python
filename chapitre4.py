69a551893287aecc06f2fecbee74468bbffb09e3
Arthur <150455953+arthurthiery@users.noreply.github.com>
2023-11-24 14:35:14 +0100

    Create chapitre4.py

---
+++ chapitre4.py
@@ -0,0 +1,34 @@
+#COURS DU 24/11
+
+
+# Exercie 1
+ma_liste = [5, 3, 9, 1, 6]
+
+ma_liste.append(7)
+ma_liste.append(2)
+
+ma_liste.sort()
+
+print(ma_liste)
+
+
+# Exercie 2
+personne = {
+    "nom": "Arthur Thiery",
+    "âge": 20,
+    "adresse": "24 Rue Jean Hameau, 33300 Bordeaux"
+}
+
+print(personne)
+
+
+# Exercie 3
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+union = ensemble1.union(ensemble2)
+
+intersection = ensemble1.intersection(ensemble2)
+
+print("Union :", union)
+print("Intersection :", intersection)
