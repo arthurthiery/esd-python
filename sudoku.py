973ff35920e5faeb328da2c9002bda3536afdfca
Arthur <150455953+arthurthiery@users.noreply.github.com>
2024-01-08 18:57:54 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,12 @@
+def generation_sudoku():
+    grille = []
+    numero_depart = 1
+    for i in range(9):
+        row = []
+        for j in range(9):
+            row.append((i*3 + i//3 + j + numero_depart - 1) % 9 + 1)
+        grille.append(row)
+    return grille
 
+for row in generation_sudoku():
+    print(row)
